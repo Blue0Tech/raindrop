027c580e5d153b9a5ecf84227cccab64f6c63c57
Pruthvi Shrikaanth <shrikaanthpruthvispareemail@gmail.com>
2020-02-22 17:39:36 +0000

    Finished rain

--- sketch.js
+++ sketch.js
@@ -1,9 +1,42 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var rain;
+var l,i,c;
+var interval, time, droplimit;
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(600,600);
+  engine = Engine.create();
+  world = engine.world;
+  rain = [];
+  i = 0;
+  c = 0;
+  droplimit = 150; //to prevent game slowing down
+  l = rain.length;
+  interval = 1; //can be tweaked to change rain spawn rate
+  time = 0;
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(0,0,0);
+  Engine.update(engine);
+  l = rain.length;
+  while(i<l) {
+    rain[i].display();
+    i++;
+  }
+  if(i>=l) {
+    i = 0;
+  }
+  if(time>=interval) {
+    time = 0;
+    console.log("time");
+    rain.push(new Raindrop(random()*600,random()*600,random()*20));
+  }
+  if(c>=droplimit) {
+    rain.shift();
+  }
+  time++;
+  c++;
 }
